13b64bebe07a4225d81543fd3da9d5253085fd2a
aitnev <aitornev@ucm.es>
2026-04-05 06:19:28 +0200

    sonos web fixing deployment ws problems

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   apiUrl: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080",
-  wsUrl: process.env.NEXT_PUBLIC_WS_URL ?? "ws://localhost:8080",
+  wsUrl: process.env.NEXT_PUBLIC_WS_URL ?? "ws://localhost:8080/ws",
   dynamicEnvId: process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID ?? "",
   platformEthAddress: process.env.NEXT_PUBLIC_PLATFORM_ETH_ADDRESS ?? "",
 } as const;
